9021616c70850265ba33ff342bbebfff3c93efe4
nitishsharma2825 <sharnit2825@gmail.com>
2025-11-01 12:41:36 +0530

    add dynamic template serving for GET and support for POST forms

--- linux-c/01_iterative/main.c
+++ linux-c/01_iterative/main.c
@@ -1,3 +1,4 @@
+#define _GNU_SOURCE /* For asprintf() */
 #include <string.h>
 #include <stdlib.h>
 #include <stdio.h>
@@ -14,16 +15,20 @@
 #include <sys/stat.h>
 #include <ctype.h> // for tolower
 
-#define SERVER_STRING           "Server: nitishhttpd/0.1\r\n"
-#define DEFAULT_SERVER_PORT     8000
-#define REDIS_SERVER_HOST       "127.0.0.1"
-#define REDIS_SERVER_PORT       6379
+#define SERVER_STRING                   "Server: nitishhttpd/0.1\r\n"
+#define DEFAULT_SERVER_PORT             8000
+#define REDIS_SERVER_HOST               "127.0.0.1"
+#define REDIS_SERVER_PORT               6379
 
-#define METHOD_HANDLED          0
-#define METHOD_NOT_HANDLED      1
+#define METHOD_HANDLED                  0
+#define METHOD_NOT_HANDLED              1
 
-#define GUESTBOOK_ROUTE         "/guestbook"
-#define GUESTBOOK_TEMPLATE      "templates/guestbook/index.html"
+#define GUESTBOOK_ROUTE                 "/guestbook"
+#define GUESTBOOK_TEMPLATE              "templates/guestbook/index.html"
+#define GUESTBOOK_REDIS_VISITOR_KEY     "visitor_count"
+#define GUESTBOOK_REDIS_REMARKS_KEY     "guestbook_remarks"
+#define GUESTBOOK_TMPL_VISITOR          "$VISITOR_COUNT$"
+#define GUESTBOOK_TMPL_REMARKS          "$GUEST_REMARKS$"
 
 const char *unimplemented_content = \
         "HTTP/1.0 400 Bad Request\r\n"
@@ -77,12 +82,56 @@ const char* get_filename_ext(const char* filename)
     return dot + 1;
 }
 
+/* Converts a hex character to its integer value */
+char from_hex(char ch)
+{
+    return isdigit(ch) ? ch - '0' : tolower(ch) - 'a' + 10;
+}
+
 /*
     Sends "HTTP Not Found" code and message to the client
 */
 void handle_http_404(int client_socket)
 {
     send(client_socket, http_404_content, strlen(http_404_content), 0);
+}
+
+/*
+    HTML URls and other data like data sent over POST method are encoded using a simple schema
+    This functions take encoded data and returns a regular, decoded string
+    eg:
+    Encoded: Nothing+is+better+than+bread+%26+butter%21
+    Decoded: Nothing is better than bread & butter!
+*/
+char* urlencoding_decode(char* str)
+{
+    char* pstr = str;
+    char* buf = malloc(strlen(str) + 1);
+    char* pbuf = buf;
+
+    while (*pstr)
+    {
+        if(*pstr == '%')
+        {
+            if(pstr[1] && pstr[2])
+            {
+                *pbuf++ = from_hex(pstr[1]) << 4 | from_hex(pstr[2]);
+                pstr += 2;
+            }
+        }
+        else if (*pstr == '+')
+        {
+            *pbuf++ = ' ';
+        }
+        else
+        {
+            *pbuf++ = *pstr;
+        }
+        pstr++;
+    }
+    *pbuf = '\0';
+
+    return buf;
 }
 
 // create a client socket for redis
@@ -106,6 +155,194 @@ void connect_to_redis_server()
     int cret = connect(redis_socket_fd, (struct sockaddr *)&redis_srvaddr, sizeof(redis_srvaddr));
     if (cret == -1) fatal_error("redis connect()");
     else printf("Connected to Redis server@ %s:%d\n", redis_host_ip, REDIS_SERVER_PORT);
+}
+
+/*
+    Internal function. It sends the command to the server,
+    but reads back the raw server response. Not very useful to be used directly without
+    first processing it to extract the required data
+*/
+int _redis_get_key(const char* key, char* value_buffer, int value_buffer_sz)
+{
+    char *req_buffer;
+    /*
+        asprintf() is a useful GNU extension that allocates the string as required
+        No more guessing the right size for the buffer that holds the string
+        Don't forget to call free() once done
+    */
+   asprintf(&req_buffer, "*2\r\n$3\r\nGET\r\n$%ld\r\n%s\r\n", strlen(key), key);
+   write(redis_socket_fd, req_buffer, strlen(req_buffer));
+   free(req_buffer);
+   read(redis_socket_fd, value_buffer, value_buffer_sz);
+   return 0;
+}
+
+/*
+    Given the key, fetch the number value associated with it
+*/
+int redis_get_int_key(const char* key, int *value)
+{
+    /*
+     * Example response from server:
+     * $3\r\n385\r\n
+     * This means that the server is telling us to expect a string
+     * of 3 characters.
+    */
+    
+    char redis_response[64] = "";
+    _redis_get_key(key, redis_response, sizeof(redis_response));
+    char* p = redis_response;
+    if (*p != '$') return -1;
+
+    while(*p++ != '\n');
+
+    /* Convert string representation of a number to a number */
+    int intval = 0;
+    while (*p != '\r')
+    {
+        intval = (intval * 10) + (*p - '0');
+        p++;
+    }
+    *value = intval;
+
+    return 0;
+}
+
+/* Increment given key by incr_by. Key is created by Redis if it doesn't exist */
+int redis_incr_by(char* key, int incr_by)
+{
+    char cmd_buf[1024] = "";
+    char incr_by_str[16] = "";
+    sprintf(incr_by_str, "%d", incr_by);
+    sprintf(cmd_buf, "*3\r\n$6\r\nINCRBY\r\n$%ld\r\n%s\r\n$%ld\r\n%d\r\n", strlen(key), key, strlen(incr_by_str), incr_by);
+    write(redis_socket_fd, cmd_buf, strlen(cmd_buf));
+    bzero(cmd_buf, sizeof(cmd_buf));
+    read(redis_socket_fd, cmd_buf, sizeof(cmd_buf));
+    return 0;
+}
+
+/* Increment value of key in redis by 1 */
+int redis_incr(char* key)
+{
+    return redis_incr_by(key, 1);
+}
+
+/*
+    Appends an item pointed to by 'value' to the list in redis referred by 'key'
+    Uses the redis RPUSH command
+*/
+int redis_list_append(char* key, char* value)
+{
+    char cmd_buf[1024] = "";
+    sprintf(cmd_buf, "*3\r\n$5\r\nRPUSH\r\n$%ld\r\n%s\r\n$%ld\r\n%s\r\n", strlen(key), key, strlen(value), value);
+    write(redis_socket_fd, cmd_buf, strlen(cmd_buf));
+    bzero(cmd_buf, sizeof(cmd_buf));
+    read(redis_socket_fd, cmd_buf, sizeof(cmd_buf));
+    return 0;
+}
+
+/*
+    Get range of items in a list from 'start' to 'end'
+    This function allocates memory. An array of pointers and all strings pointed to by it are dynamically allocated.
+*/
+int redis_list_get_range(char* key, int start, int end, char*** items, int* items_count)
+{
+    /*
+     *  The Redis protocol is elegantly simple. The following is a response for an array
+     *  that has 3 elements (strings):
+     *  Example response:
+     *  *3\r\n$5\r\nHello\r\n$6\r\nLovely\r\n\$5\r\nWorld\r\n
+     *
+     *  What it means:
+     *  *3      -> Array with 3 items
+     *  $5      -> string with 5 characters
+     *  Hello   -> actual string
+     *  $6      -> string with 6 characters
+     *  Lovely  -> actual string
+     *  $5      -> string with 5 characters
+     *  World   -> actual string
+     *
+     *  A '\r\n' (carriage return + line feed) sequence is used as the delimiter.
+     *  Now, you should be able to understand why we're doing what we're doing in this function
+     * */
+
+    char cmd_buf[1024]="", start_str[16], end_str[16];
+    sprintf(start_str, "%d", start);
+    sprintf(end_str, "%d", end);
+    sprintf(cmd_buf, "*4\r\n$6\r\nLRANGE\r\n$%ld\r\n%s\r\n$%ld\r\n%s\r\n$%ld\r\n%s\r\n", strlen(key), key, strlen(start_str), start_str, strlen(end_str), end_str);
+    write(redis_socket_fd, cmd_buf, strlen(cmd_buf));
+
+    /* Find the length of the returned array */
+    char ch;
+    read(redis_socket_fd, &ch, 1);
+    if (ch != '*') return -1;
+
+    int returned_items = 0;
+    while(1)
+    {
+        read(redis_socket_fd, &ch, 1);
+        if (ch == '\r')
+        {
+            // read the next \n char
+            read(redis_socket_fd, &ch, 1);
+            break;
+        }
+        returned_items = (returned_items * 10) + (ch - '0');
+    }
+
+    *items_count = returned_items;
+    /* Allocate array that will hold pointer each for every element in the returned list */
+    char** items_holder = malloc(sizeof(char*) * returned_items);
+    *items = items_holder;
+
+    /*
+        We know length of array. Loop that many iterations and grap those strings
+        allocating a new chunk of memory for each
+    */
+    for (int i = 0; i < returned_items; i++)
+    {
+        // read the fist $
+        read(redis_socket_fd, &ch, 1);
+        int str_size = 0;
+        while(1)
+        {
+            read(redis_socket_fd, &ch, 1);
+            if (ch == '\r')
+            {
+                read(redis_socket_fd, &ch, 1);
+                break;
+            }
+            str_size = (str_size * 10) + (ch - '0');
+        }
+
+        // allocate and read the string
+        char *str = malloc(sizeof(char) * str_size + 1);
+        items_holder[i] = str;
+        read(redis_socket_fd, str, str_size);
+        str[str_size] = '\0';
+
+        // Read the '\r\n' chars
+        read(redis_socket_fd, &ch, 1);
+        read(redis_socket_fd, &ch, 1);
+    }
+}
+
+/* Free all dynamically allocated string */
+int redis_free_array_result(char** items, int length)
+{
+    for (int i = 0; i < length; i++)
+    {
+        free(items[i]);
+    }
+    free(items);
+}
+
+/*
+    Utility function to get the whole list
+*/
+int redis_get_list(char* key, char*** items, int* items_count)
+{
+    return redis_list_get_range(key, 0, -1, items, items_count);
 }
 
 // creates a server socket, defines a socket address
@@ -233,8 +470,9 @@ void send_headers(const char* path, off_t len, int client_socket)
 */
 int render_guestbook_template(int client_socket)
 {
-    char templ[16384];
-    char rendering[16384];
+    /* safe programming, all offsets are set to \0, else they are filled with garbage. */
+    char templ[16384] = "";
+    char rendering[16384] = "";
 
     /* Read the template file*/
     int fd = open(GUESTBOOK_TEMPLATE, O_RDONLY);
@@ -243,6 +481,68 @@ int render_guestbook_template(int client_socket)
     close(fd);
 
     /* Get guestbook entries and render them as HTML */
+    int entries_count;
+    char** guest_entries;
+    char guest_entries_html[16384] = "";
+
+    redis_get_list(GUESTBOOK_REDIS_REMARKS_KEY, &guest_entries, &entries_count);
+    for (int i = 0; i < entries_count; i++)
+    {
+        char guest_entry[1024];
+        sprintf(guest_entry, "<p class=\"guest-entry\">%s</p>", guest_entries[i]);
+        strcat(guest_entries_html, guest_entry);
+    }
+    redis_free_array_result(guest_entries, entries_count);
+
+    /* In Redis, increment visitor count and fetch latest value */
+    int visitor_count;
+    char visitor_count_str[16] = "";
+    redis_incr(GUESTBOOK_REDIS_VISITOR_KEY);
+    redis_get_int_key(GUESTBOOK_REDIS_VISITOR_KEY, &visitor_count);
+    sprintf(visitor_count_str, "%'d", visitor_count);
+
+    /* Replace guestbook entries in HTML*/
+    char *entries = strstr(templ, GUESTBOOK_TMPL_REMARKS);
+    if (entries)
+    {
+        memcpy(rendering, templ, entries - templ);
+        strcat(rendering, guest_entries_html);
+        char* copy_offset = templ + (entries - templ) + strlen(GUESTBOOK_TMPL_REMARKS);
+        strcat(rendering, copy_offset);
+        strcpy(templ, rendering);
+        bzero(rendering, sizeof(rendering));
+    }
+
+    /* Replace visitor count in HTML*/
+    char* vcount = strstr(templ, GUESTBOOK_TMPL_VISITOR);
+    if (vcount)
+    {
+        memcpy(rendering, templ, vcount - templ);
+        strcat(rendering, visitor_count_str);
+        char* copy_offset = templ + (vcount - templ) + strlen(GUESTBOOK_TMPL_VISITOR);
+        strcat(rendering, copy_offset);
+        strcpy(templ, rendering);
+        bzero(rendering, sizeof(rendering));
+    }
+
+    /*
+        Template is rendered, Send headers and template over to the client
+    */
+    char send_buffer[1024];
+    strcpy(send_buffer, "HTTP/1.0 200 OK\r\n");
+    send(client_socket, send_buffer, strlen(send_buffer), 0);
+    strcpy(send_buffer, SERVER_STRING);
+    send(client_socket, send_buffer, strlen(send_buffer), 0);
+    strcpy(send_buffer, "Content-Type: text/html\r\n");
+    send(client_socket, send_buffer, strlen(send_buffer), 0);
+    sprintf(send_buffer, "content-length: %ld\r\n", strlen(templ));
+    send(client_socket, send_buffer, strlen(send_buffer), 0);
+    strcpy(send_buffer, "\r\n");
+    send(client_socket, send_buffer, strlen(send_buffer), 0);
+
+    // send template
+    send(client_socket, templ, strlen(templ), 0);
+    printf("200 GET /guestbook %ld bytes\n", strlen(templ));
 }
 
 /*
@@ -316,19 +616,127 @@ void handle_get_method(char* path, int client_socket)
     }
 }
 
+/*
+    Guest submits name and remarks via the form on the page.
+    That data is available to us as post x-www-form-urlencoded data.
+    Need to decode and get what we need in plain text.
+
+    At this point, we have already read the headers and whats left is post data, which forms the body of request
+*/
+void handle_new_guest_remarks(int client_socket)
+{
+    char remarks[1024] = "";
+    char name[512] = "";
+    char buffer[4026] = "";
+    char* c1;
+    char* c2;
+    read(client_socket, buffer, sizeof(buffer));
+    /*
+     * Sample data format:
+     * guest-remarks=Relatively+great+service&guest-name=Albert+Einstein
+     * */
+
+    char* assignment = strtok_r(buffer, "&", &c1);
+    do
+    {
+        char* subassignment = strtok_r(assignment, "=", &c2);
+        if (!subassignment) break;
+        
+        do
+        {
+            if (strcmp(subassignment, "guest-name") == 0)
+            {
+                subassignment = strtok_r(NULL, "=", &c2);
+                if (!subassignment)
+                {
+                    name[0] = '\0';
+                    break;
+                }
+                else
+                {
+                    strcpy(name, subassignment);
+                }
+            }
+
+            if (strcmp(subassignment, "guest-remarks") == 0)
+            {
+                subassignment = strtok_r(NULL, "=", &c2);
+                if(!subassignment)
+                {
+                    remarks[0] = '\0';
+                    break;
+                }
+                else
+                {
+                    strcpy(remarks, subassignment);
+                }
+            }
+            subassignment = strtok_r(NULL, "=", &c2);
+            if (!subassignment) break;
+        } while (1);
+
+        assignment = strtok_r(NULL, "&", &c1);
+        if (!assignment) break;
+    } while (1);
+
+    /* Validate name and remark lenghts and show an error page if required */
+    if(strlen(name) == 0 || strlen(remarks) == 0)
+    {
+        char* html = "HTTP/1.0 400 Bad Request\r\ncontent-type: text/html\r\n\r\n<html><title>Error</title><body><p>Error: Do not leave name or remarks empty.</p><p><a href=\"/guestbook\">Go back to Guestbook</a></p></body></html>";
+        write(client_socket, html, strlen(html));
+        printf("400 POST /guestbook\n");
+        return;
+    }
+
+    /*
+        POST uses form URL encoding. Decode the strings and append them to the Redis
+        list that holds all remarks.
+    */
+   char* decoded_name = urlencoding_decode(name);
+   char* decoded_remarks = urlencoding_decode(remarks);
+   bzero(buffer, sizeof(buffer));
+   sprintf(buffer, "%s - %s", decoded_remarks, decoded_name);
+   redis_list_append(GUESTBOOK_REDIS_REMARKS_KEY, buffer);
+   free(decoded_name);
+   free(decoded_remarks);
+
+   /* All good! Show a 'thank you' page. */
+   char *html = "HTTP/1.0 200 OK\r\ncontent-type: text/html\r\n\r\n<html><title>Thank you!</title><body><p>Thank you for leaving feedback! We really appreciate that!</p><p><a href=\"/guestbook\">Go back to Guestbook</a></p></body></html>";
+   write(client_socket, html, strlen(html));
+   printf("200 POST /guestbook\n");
+}
+
+/*
+    This is the routing function for POST calls,
+    Can be extended by adding newer POST methods and its handlers
+*/
+int handle_app_post_routes(char* path, int client_socket)
+{
+    if (strcmp(path, GUESTBOOK_ROUTE) == 0)
+    {
+        handle_new_guest_remarks(client_socket);
+        return METHOD_HANDLED;
+    }
+
+    // add new app routes here
+    return METHOD_NOT_HANDLED;
+}
+
 void handle_post_method(char* path, int client_socket)
 {
-    printf("post method handler");
+    // it can only be for app methods
+    handle_app_post_routes(path, client_socket);
 }
 
 void handle_unimplemented_method(int client_socket)
 {
-    printf("unimplemented method handler");
+    send(client_socket, unimplemented_content, strlen(unimplemented_content), 0);
 }
 
 void handle_http_method(char* method_buffer, int client_socket)
 {
-    char* method, *path;
+    char* method;
+    char* path;
     method = strtok(method_buffer, " ");
     strtolower(method);
     path = strtok(NULL, " ");
@@ -361,7 +769,7 @@ void handle_client(int client_socket)
         unsigned long len = strlen(line_buffer);
 
         // 1st line has HTTP method, we only care about that
-        // we read rest of the lines and throw them away
+        // we read rest of the header lines and throw them away
         if (method_line == 1)
         {
             if (len == 0) return;
@@ -389,6 +797,7 @@ void enter_server_loop(int server_socket)
         int client_socket = accept(server_socket, (struct sockaddr *)&client_addr, &client_addr_len);
         if (client_socket == -1) fatal_error("accept()");
 
+        // handles only 1 request per client connection right now
         handle_client(client_socket);
         close(client_socket);
     }
